ce1a03ed91cb06207651eb2046c05ae158a7e65a
chalalala <neko_hime1212@yahoo.com>
2020-10-10 08:54:28 +0700

    Tidy the code

--- src/App.js
+++ src/App.js
@@ -25,81 +25,81 @@ export default class App extends Component {
   render(){
     return (
       <Router>
-      <div className="App">
-        <div id="sidebar">
-          <div
-            className="sidebar"
-            style={{display: this.state.menuOpen ? 'none' : 'block'}}>
+        <div className="App">
+          <div id="sidebar">
+            <div
+              className="sidebar"
+              style={{display: this.state.menuOpen ? 'none' : 'block'}}>
+            </div>
+            <Menu
+              isOpen={this.state.menuOpen}
+              onStateChange={(state) => this.handleStateChange(state)}
+            >
+              <Navbar.Text style={styles.label}>Menu</Navbar.Text>
+                <NavLink to="/dashboard" onClick={() => this.toggleMenu(false)} className="link" activeClassName="activeLink">
+                  Dashboard
+                </NavLink>
+                <NavLink to="/analytics" onClick={() => this.toggleMenu(false)} className="link" activeClassName="activeLink">
+                Analytics
+                </NavLink> 
+              <Navbar.Text style={styles.label}>Charts</Navbar.Text>
+            </Menu>
           </div>
-          <Menu
-            isOpen={this.state.menuOpen}
-            onStateChange={(state) => this.handleStateChange(state)}
-          >
-            <Navbar.Text style={styles.label}>Menu</Navbar.Text>
-              <NavLink to="/dashboard" onClick={() => this.toggleMenu(false)} className="link" activeClassName="activeLink">
-                Dashboard
-              </NavLink>
-              <NavLink to="/analytics" onClick={() => this.toggleMenu(false)} className="link" activeClassName="activeLink">
-               Analytics
-              </NavLink> 
-            <Navbar.Text style={styles.label}>Charts</Navbar.Text>
-          </Menu>
+          
+          <div className="content">
+            <Navbar variant="light" style={{marginTop: '-1.7em', marginRight: '-4em'}}>
+              <Navbar.Brand href="#">
+                <img src={require("./HDLogo.png")} width="100" crop="scale"
+                className="d-inline-block align-top"/>
+              </Navbar.Brand>
+              
+              <div id="header-nav">
+                <Nav className="mr-auto">
+                  {/* <FontAwesomeIcon icon={faAccusoft}  size="1x" color="#0A4877"/> */}
+                  <Nav.Link >&nbsp;</Nav.Link>
+                  <NavDropdown title="Home" className="nav-item">
+                    <NavDropdown.Item href ="#"> dunno1</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Setting" className="nav-item">
+                    <NavDropdown.Item href ="#"> dunno2</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Projects" className="nav-item">
+                    <NavDropdown.Item href ="#"> dunno3</NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link className="nav-item">
+                    <FontAwesomeIcon icon={faComments}/>
+                    <span>Contact</span>
+                  </Nav.Link>
+                </Nav>
+              </div>
+
+              <Navbar className="ml-auto">
+                <Nav.Link>
+                  <div class="icon-wrapper-bg bg-danger">
+                    <FontAwesomeIcon icon={faBell}  className="iconNav" size="lg"/>
+                  </div>
+                </Nav.Link>
+                <Image src={require("./3.jpg")} roundedCircle className="small-avatar"/>
+                <Navbar.Text style={{padding:0,textAlign:"left"}}>
+                  <h6 style={{marginBottom:0}}>Phuong Trinh</h6>
+                  <div>Active</div>
+                </Navbar.Text>
+              </Navbar>
+            </Navbar>
+
+            <Switch>
+              <Route path="/dashboard">
+                <Dashboard/>
+              </Route>
+              <Route path="/analytics">
+                <Analytics/>
+              </Route>
+              <Route path="/">
+                <Dashboard/>
+              </Route>
+            </Switch>
+          </div>   
         </div>
-        
-      
-      
-      <div className="content">
-      <Navbar variant="light" style={{marginTop: '-1.7em', marginRight: '-4em'}}>
-        <Navbar.Brand href="#">
-        <img src={require("./HDLogo.png")} width="100" crop="scale"
-          className="d-inline-block align-top"/>
-        </Navbar.Brand>
-        <div id="header-nav">
-        <Nav className="mr-auto">
-          {/* <FontAwesomeIcon icon={faAccusoft}  size="1x" color="#0A4877"/> */}
-          <Nav.Link >&nbsp;</Nav.Link>
-          <NavDropdown title="Home" className="nav-item">
-            <NavDropdown.Item href ="#"> dunno1</NavDropdown.Item>
-          </NavDropdown>
-          <NavDropdown title="Setting" className="nav-item">
-            <NavDropdown.Item href ="#"> dunno2</NavDropdown.Item>
-          </NavDropdown>
-          <NavDropdown title="Projects" className="nav-item">
-            <NavDropdown.Item href ="#"> dunno3</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link className="nav-item">
-              <FontAwesomeIcon icon={faComments}/>
-            <span>Contact</span>
-          </Nav.Link>
-        </Nav>
-        </div>
-        <Navbar className="ml-auto">
-          <Nav.Link>
-          <div class="icon-wrapper-bg bg-danger">
-            <FontAwesomeIcon icon={faBell}  className="iconNav" size="lg"/>
-          </div>
-          </Nav.Link>
-          <Image src={require("./3.jpg")} roundedCircle className="small-avatar"/>
-          <Navbar.Text style={{padding:0,textAlign:"left"}}>
-            <h6 style={{marginBottom:0}}>Phuong Trinh</h6>
-             <div>Active</div>
-            </Navbar.Text>
-        </Navbar>
-      </Navbar>
-          <Switch>
-            <Route path="/dashboard">
-              <Dashboard/>
-            </Route>
-            <Route path="/analytics">
-              <Analytics/>
-            </Route>
-            <Route path="/">
-              <Dashboard/>
-            </Route>
-          </Switch>
-        </div>
-        
-      </div>
       </Router>
     );
   }
