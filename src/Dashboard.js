ca9482e96a34959ba6dbb8a44252d239d1a73b4d
chalalala <neko_hime1212@yahoo.com>
2020-10-14 09:10:58 +0700

    Add portions in pie

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,19 +5,25 @@ import { Row, Col, } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const activeUsers = {
-    labels: ['Active Users', 'Inactive Users'],
+    labels: ['12-18', '19-30','30-60','>60'],
     datasets: [
         {
         label: 'Rainfall',
         backgroundColor: [
           '#B21F00',
-          '#d9e4dd',
+          '#C9DE00',
+          '#2FDE00',
+          '#00A6B4',
+          '#6800B4'
         ],
         hoverBackgroundColor: [
-          '#501800',
-          '#cdc9c3'
+        '#501800',
+        '#4B5000',
+        '#175000',
+        '#003350',
+        '#35014F'
         ],
-        data: [65, 35]
+        data: [65, 59, 80, 81, 56]
         }
     ]
 };
@@ -33,11 +39,11 @@ export default class Dashboard extends Component{
               height={200}
               data={activeUsers}
               options={{
-                // title:{
-                //   display:true,
-                //   text:'Something',
-                //   fontSize:20
-                // },
+                title:{
+                  display:true,
+                  text:'Ages',
+                  fontSize:20
+                },
                 legend:{
                   display:true,
                   position:'bottom'
