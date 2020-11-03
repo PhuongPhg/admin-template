e239c44257b8cd51c31c63a4fc52c11c0c38ad99
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-11-04 00:56:40 +0700

    Day la mot cuoc cai cach nen co qua nhieu thu de commit

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import styled from 'styled-components';
+const Styles = styled.div`
+  .navbar { 
+    background-color: 'transparent'; 
+  }
+  a, .navbar-nav, .navbar-light .nav-link {
+    color: white;
+    &:hover { color: white}
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    color: white;
+    &:hover { font-weight: 'bold' };
+  }
+  .form-center {
+    position: absolute !important;
+    left: 25%;
+    right: 25%;
+  }
+  .cl-effect-1 a::before, .cl-effect-1 a::after {
+	display: inline-block;
+	opacity: 0;
+	-webkit-transition: -webkit-transform 0.3s, opacity 0.2s;
+	-moz-transition: -moz-transform 0.3s, opacity 0.2s;
+	transition: transform 0.3s, opacity 0.2s;
+}
+
+.cl-effect-1 a::before {
+	margin-right: 10px;
+	content: '[';
+	-webkit-transform: translateX(20px);
+	-moz-transform: translateX(20px);
+	transform: translateX(20px);
+}
+
+.cl-effect-1 a::after {
+	margin-left: 10px;
+	content: ']';
+	-webkit-transform: translateX(-20px);
+	-moz-transform: translateX(-20px);
+	transform: translateX(-20px);
+}
+
+.cl-effect-1 a:hover::before,
+.cl-effect-1 a:hover::after,
+.cl-effect-1 a:focus::before,
+.cl-effect-1 a:focus::after {
+	opacity: 1;
+	-webkit-transform: translateX(0px);
+	-moz-transform: translateX(0px);
+	transform: translateX(0px);
+}
+`;
+export const NavigationBar = () => (
+  <Styles >
+    <Navbar expand="lg" >
+      <Navbar.Brand href="/">   
+        <img src={require("../HDLogo.png")} width="100" crop="scale"
+              className="d-inline-block align-top"/>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      {/* <Form className="form-center">
+        <FormControl type="text" placeholder="Search" className="" />
+      </Form> */}
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto cl-effect-1">
+          {/* <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>  */}
+          <Nav.Item><Nav.Link href="/about" >Setting</Nav.Link></Nav.Item>
+          <div style={{flexDirection: 'row', }}>
+            <img src={user.avatar} width="50" crop="scale" style={{borderRadius: 25}}
+              className="d-inline-block align-top"/>
+              <Navbar.Text style={{padding:0,textAlign:"left", color: 'white', marginLeft: 10}}>
+              <h6 style={{marginBottom:0}}>{user.username}</h6>
+              <div>{user.status}</div>
+              {/* <Nav.Link href="#" >{user.username}</Nav.Link> */}
+              </Navbar.Text>  
+          </div>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+)
+const user = {
+  username: 'Phuong Trinh',
+  avatar: require("../3.jpg"),
+  status: 'Active'
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import Dashboard from '../Dashboard.js';
+// import { Link } from 'react-router'
+
+const StyledSideNav = styled.div`
+  position: fixed;     /* Fixed Sidebar (stay in place on scroll and position relative to viewport) */
+  height: 100%;
+  width: 75px;     /* Set the width of the sidebar */
+  z-index: 1;      /* Stay on top of everything */
+  top: 3.4em;      /* Stay at the top */
+//   background-color: green;
+  overflow-x: hidden;     /* Disable horizontal scroll */
+  padding-top: 10px;
+  color: white;
+  
+`;
+
+export default class Sidebar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { hover: false };
+    }
+    render() {
+    return (
+        <div>
+        <StyledSideNav>
+            <div style={{display: 'flex', flexDirection: "column"}}>
+                <div style={{padding: 10, textAlign: 'center', marginBottom: 10}}>
+                    <Link to='/'>
+                        <FontAwesomeIcon icon={faHome} size="3x" color=" rgba(255,255,255,0.7)" 
+                        onMouseOver={() => this.setState({hover: true})}
+                        onMouseLeave={() =>  this.setState({hover: false})}
+                        style={this.state.hover ? { color: "white" } : {}}
+                        />
+                    </Link>
+                </div>
+                <div style={{padding: 10, textAlign: "center"}}>
+                    <Link to='/about'>
+                        <FontAwesomeIcon icon={faUser} size="3x" color=" rgba(255,255,255,0.7)" 
+                        onMouseOver={() => this.setState({hover: true})}
+                        onMouseLeave={() =>  this.setState({hover: false})}
+                        style={this.state.hover ? { color: "white" } : {}}
+                        />
+                    </Link>
+                </div>
+            </div>
+        </StyledSideNav>
+        <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/about" component={item2} />
+      </Switch>
+      </div>
+    );
+  }
+}
+function item2(){
+    return <p>item22222222</p>
+  }
